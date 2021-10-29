002150d9f8a93b1140242b4cbbfb1f007fd401d3
crater90 <kamranmohd89@gmail.com>
2021-10-29 14:41:28 +0530

    image update

--- src/components/Landing/Skillscard.tsx
+++ src/components/Landing/Skillscard.tsx
@@ -4,7 +4,7 @@ const Skillscard = ({name, image, link}) => {
     return (
        <div>
            <a href= {link}>
-              <div className="flex items-center justify-start gap-10 py-4 px-6 border border-gray-200 rounded-md hover:-translate-y-2 hover:shadow-xl transition transform duration-150 ease-out active:scale-95">
+              <div className="flex items-center justify-start gap-10 py-4 px-6 border border-gray-400 hover:border-gray-600  dark:border-gray-600 dark:hover:border-gray-400 rounded-md hover:-translate-y-2 hover:shadow-xl transition transform duration-150 ease-out active:scale-95">
                   <div className="flex items-center justify-center w-11 h-11 rounded-md bg-blue-50">
                       <img className="w-8 h-8" src ={image}/>
                   </div>               
